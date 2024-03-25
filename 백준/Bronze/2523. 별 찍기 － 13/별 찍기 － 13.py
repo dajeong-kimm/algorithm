4780c208aa7c15c3e9c1ce4e9c8c993cb2617af5
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-03-25 22:15:26 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for i in range(1,n+1):
+    star = []
+    for _ in range(i):
+        star.append('*')
+    print(''.join(star))
+for i in range(n-1,0,-1):
+    star = []
+    for _ in range(i):
+        star.append('*')
+    print(''.join(star))
