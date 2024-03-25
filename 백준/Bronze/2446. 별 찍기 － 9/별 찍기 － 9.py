18a455a79257b7186a3838e2f9832813f10caa27
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-03-25 22:24:50 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(n,0,-1):
+    star = []
+    for _ in range(n-i):
+        star.append(' ')
+    for _ in range(i*2-1):
+        star.append('*')
+    print(''.join(star))
+for i in range(2,n+1):
+    star = []
+    for _ in range(n-i):
+        star.append(' ')
+    for _ in range(i*2-1):
+        star.append('*')
+    print(''.join(star))
