1218b71cd20b0d0ed427e4de626b8587e03c011c
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-03-25 19:49:28 +0900

    [Bronze II] Title: 첼시를 도와줘!, Time: 364 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11098. 첼시를 도와줘！/첼시를 도와줘！.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for _ in range(n):
+    m = int(input())
+    player = []
+    for _ in range(m):
+        player.append(list(map(str, input().split())))
+        player = sorted(player, key = lambda x:int(x[0]),reverse=True)
+    print(player[0][1])
