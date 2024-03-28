f9a9e896906cdac0228f292a7de83270af60fe41
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-03-29 06:59:41 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 780 ms, Memory: 93316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,11 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-s = list(input().rstrip())
-q = int(input())
-for _ in range(q):
-    a,start,end = map(str,input().split())
-    start,end = int(start), int(end)
+# print(ord('a'))
+s = input().strip()
+count = [[0]*26]
 
-    answer = s[start:end+1].count(a)
+q = int(input())
+
+for i, ch in enumerate(s):
+    count.append(count[len(count)-1][:])
+    count[i+1][ord(ch)-97] += 1
+
+for _ in range(q):
+    alpha, l, r = input().split()
+    answer = count[int(r)+1][ord(alpha)-97] - count[int(l)][ord(alpha)-97]
+
     print(answer)
