babf1cdd0d431dbb74d9f3e78808fa3bc7719156
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-04-13 20:48:50 +0900

    [Silver I] Title: 기타 레슨, Time: 152 ms, Memory: 120932 KB -BaekjoonHub

---
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.py
@@ -0,0 +1,22 @@
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+
+start = max(arr)
+end = sum(arr)
+
+while start<=end:
+    mid = (start+end)//2
+
+    total = 0
+    count = 1
+    for t in arr:
+        if total+t>mid:
+            count += 1
+            total = 0
+        total += t
+    if count<=m:
+        ans = mid
+        end = mid-1
+    else:
+        start = mid+1
+print(ans)
