d3d4b05d512d343604b340960ca47cdd5af38722
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-04-14 21:44:41 +0900

    [Gold V] Title: 치킨 배달, Time: 220 ms, Memory: 112752 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -0,0 +1,39 @@
+n,m = map(int,input().split())
+
+min_ans = 99999999
+
+home = []
+chick = []
+
+for i in range(n):
+    row = list(map(int,input().split()))
+    for j in range(n):
+        if row[j] == 1:
+            home.append((i,j))
+        elif row[j] ==2:
+            chick.append((i,j))
+
+visited = [False]*len(chick)
+
+def dfs(idx,cnt):
+    global min_ans
+    if cnt == m:
+        ans = 0
+        for i in home:
+            distance = 99999999
+            for j in range(len(visited)):
+                if visited[j]:
+                    check_num = abs(i[0]-chick[j][0])+abs(i[1]-chick[j][1])
+                    distance = min(distance,check_num)
+            ans += distance
+        min_ans = min(ans,min_ans)
+        return
+    
+    for i in range(idx,len(chick)):
+        if not visited[i]:
+            visited[i] = True
+            dfs(i+1,cnt+1)
+            visited[i] = False
+
+dfs(0,0)
+print(min_ans)
