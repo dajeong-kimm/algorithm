2a840badf8cb0067960817c24940f448807f1aaf
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-04-16 14:56:06 +0900

    [Gold III] Title: 소수의 연속합, Time: 452 ms, Memory: 177572 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -1,15 +1,12 @@
 def prime(num):
     arr = []
-    if num == 1:
-        return []
-    for i in range(2,num+1):
-        flag = True
-        for j in range(2,int(i**0.5)+1):
-            if i%j == 0:
-                flag = False
-                break
-        if flag==True:
+    a = [False,False] + [True]*(n-1)
+    
+    for i in range(2,n+1):
+        if a[i]:
             arr.append(i)
+            for j in range(2*i,n+1,i):
+                a[j] = False
     return arr
 
 n = int(input())
