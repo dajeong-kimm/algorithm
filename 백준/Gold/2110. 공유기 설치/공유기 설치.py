0ddee85f468a15e77aa315ee7f0849380e6f904a
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-04-16 11:15:20 +0900

    [Gold IV] Title: 공유기 설치, Time: 220 ms, Memory: 113124 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,27 @@
+n,c = map(int,input().split())
+
+arr = [int(input()) for _ in range(n)]
+
+arr.sort()
+
+start = 1
+end = arr[-1]-arr[0]
+result = 0
+
+while start<=end:
+    mid = (start+end)//2
+    current = arr[0]
+    count = 1
+
+    for i in range(1,len(arr)):
+        if arr[i]>=current+mid:
+            count += 1
+            current = arr[i]
+    
+    if count>=c:
+        start = mid+1
+        result = mid
+    else:
+        end = mid -1
+    
+print(result)
