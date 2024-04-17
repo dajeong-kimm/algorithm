f088097393b5c10a71651d9960647bb59ad63fb8
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-04-17 14:32:12 +0900

    [Bronze II] Title: 하얀 칸, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,8 @@
+board = [list(input()) for _ in range(8)]
+
+answer = 0
+for i in range(8):
+    for j in range(8):
+        if (i+j)%2 == 0 and board[i][j] == "F":
+            answer += 1
+print(answer)
