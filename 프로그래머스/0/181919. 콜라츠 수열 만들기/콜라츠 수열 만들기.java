f4072b75cff5491571c022b46ebb237e231b4a6a
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-08 13:54:38 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 3.11 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,9 @@
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int n) {
+        return IntStream
+            .concat(IntStream
+                    .iterate(n,i -> i>1, i -> i%2==0?i/2:3*i+1),IntStream.of(1))
+            .toArray();
+    }
+}
