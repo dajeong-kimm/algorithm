682ae829719c7537dfe5e96e332704faebbbd757
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-08 15:36:25 +0900

    [level 0] Title: 문자열 묶기, Time: 5.55 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String[] strArr) {
+        int answer = 0;
+        int[] lengArr = new int[31];
+        for (int i=0;i<strArr.length;i++){
+            lengArr[strArr[i].length()]++;
+        }
+        for (int i=0;i<=30;i++){
+            answer = Math.max(answer,lengArr[i]);
+        }
+        return answer;
+    }
+}
