0e088a1b29d34266abf861ef65bf8ef111b1bd66
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-08 15:32:18 +0900

    [level 0] Title: 세 개의 구분자, Time: 40.50 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String myStr) {
+        String[] arr = Arrays.stream(myStr.split("[abc]+"))
+            .filter(str -> !str.isEmpty()).toArray(String[]::new);
+        return arr.length==0 ? new String[] { "EMPTY"}:arr;
+    }
+}
