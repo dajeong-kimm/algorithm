0133df604f654c2d2f39326917e03955795c5c9f
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-22 01:29:15 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.37 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String[] answer = my_string.split("");
+        Set<String> set = new LinkedHashSet<String>(Arrays.asList(answer));
+        return String.join("",set);
+    }
+}
