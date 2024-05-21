ffff50048a9c6797007bdb6100077dce59b9d052
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-22 02:52:30 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -2,10 +2,10 @@ class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
         for (int num : num_list){
-            while (num!=1){
-                if (num%2==0) num /= 2;
+            while (num>1){
+                if (num%2 == 0) num /= 2;
                 else num = (num-1)/2;
-                answer++;
+                answer += 1;
             }
         }
         return answer;
