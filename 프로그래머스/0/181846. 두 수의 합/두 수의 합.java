5710824131e3cfa672766888519e5d386aaaf68f
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-22 07:28:49 +0900

    [level 0] Title: 두 수의 합, Time: 214.29 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,10 @@
+import java.math.*;
+class Solution {
+    public String solution(String a, String b) {
+        String answer = "";
+        BigInteger A = new BigInteger(a);
+        BigInteger B = new BigInteger(b);
+        answer += A.add(B);
+        return answer;
+    }
+}
