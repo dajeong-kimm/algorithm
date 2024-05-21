9e57e8cf21254e0cdd2f215ce2c6501234c4fd6d
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-21 23:28:22 +0900

    [level 0] Title: l로 만들기, Time: 683.15 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/README.md
+++ 프로그래머스/0/181834. l로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] l로 만들기 - 181834 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181834?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181834) 
 
 ### 성능 요약
 
-메모리: 389 MB, 시간: 636.91 ms
+메모리: 392 MB, 시간: 683.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 08일 03:47:39
+2024년 05월 21일 23:28:19
 
 ### 문제 설명
 
--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,12 +1,9 @@
 class Solution {
     public String solution(String myString) {
         String answer = "";
-        for (int i=0;i<myString.length();i++){
-            if(myString.charAt(i)<='l'){
-                answer += "l";
-            }else{
-                answer += myString.charAt(i)+"";
-            }
+        for(int i=0;i<myString.length();i++){
+            if(myString.charAt(i)<='l') answer += "l";
+            else answer += myString.charAt(i);
         }
         return answer;
     }
