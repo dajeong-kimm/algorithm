8c72952f6aae82ad48a10ee7abdb4ec52c39d598
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-22 05:49:34 +0900

    [level 0] Title: 커피 심부름, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        for(String str: order){
-            if(str.contains("cafe")) answer += 5000;
+        for (String ord : order){
+            if (ord.contains("cafe")) answer += 5000;
             else answer += 4500;
         }
         return answer;
