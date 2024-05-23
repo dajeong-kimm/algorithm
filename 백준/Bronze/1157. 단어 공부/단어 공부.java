2e94013bdb92b92e2be031c49393973bde7aae06
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-05-23 11:11:18 +0900

    [Bronze I] Title: 단어 공부, Time: 292 ms, Memory: 22996 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        input = input.toLowerCase();
+        int[] alpha = new int[26];
+        for (char c : input.toCharArray()) {
+            int tmp = (int)(c-'a');
+            alpha[tmp] += 1;
+        }
+
+        String answer = "";
+        int max_count = 0;
+        for (int i=0;i<26;i++){
+            if (max_count<alpha[i]) {
+                char tmp_char = (char)(i+'A');
+                answer = Character.toString(tmp_char);
+                max_count = alpha[i];
+            }
+        }
+        int tmp_count = 0;
+        for (int i=0;i<26;i++){
+            if (alpha[i] == max_count) tmp_count += 1;
+        }
+        if (tmp_count>1) answer = "?";
+        System.out.println(answer);
+    }
+}
