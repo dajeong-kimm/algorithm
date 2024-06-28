57462415761052f5bca40ca6582ba9b30715094b
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-06-28 23:25:32 +0900

    [Bronze I] Title: 단어 공부, Time: 188 ms, Memory: 26244 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,31 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.StringTokenizer;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String input = br.readLine();
         input = input.toLowerCase();
         int[] alpha = new int[26];
-        for (char c : input.toCharArray()) {
+
+        for(char c : input.toCharArray()){
             int tmp = (int)(c-'a');
             alpha[tmp] += 1;
         }
 
-        String answer = "";
         int max_count = 0;
+        String answer = "";
+
         for (int i=0;i<26;i++){
-            if (max_count<alpha[i]) {
+            if(max_count<alpha[i]){
                 char tmp_char = (char)(i+'A');
-                answer = Character.toString(tmp_char);
                 max_count = alpha[i];
+                answer = Character.toString(tmp_char);
             }
         }
+
         int tmp_count = 0;
         for (int i=0;i<26;i++){
             if (alpha[i] == max_count) tmp_count += 1;
         }
+
         if (tmp_count>1) answer = "?";
         System.out.println(answer);
     }
