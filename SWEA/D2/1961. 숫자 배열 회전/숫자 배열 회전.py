5db9967e7bc5e19ae0f8ea77345403e8fe775a68
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-06-29 02:35:19 +0900

    [D2] Title: 숫자 배열 회전, Time: 145 ms, Memory: 44,956 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,27 +1,59 @@
-import copy
-def rotate_arr():
-    new_arr = [[0 for _ in range(n)] for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            new_arr[j][n - 1 - i] = arr[i][j]
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-    return new_arr
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case=1;test_case<=T;test_case++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
 
-t = int(input())
-for test_case in range(1,t+1):
-    print(f"#{test_case}")
-    n = int(input())
-    arr = [list(map(str,input().split())) for _ in range(n)]
-    answer = [[0 for _ in range(3)] for _ in range(n)]
-    arr = rotate_arr()
-    for i in range(n):
-        answer[i][0] = ''.join(arr[i])
-    arr = rotate_arr()
-    for i in range(n):
-        answer[i][1] = ''.join(arr[i])
-    arr = rotate_arr()
-    for i in range(n):
-        answer[i][2] = ''.join(arr[i])
-    for ans in answer:
-        print(*ans)
+            int[][] arr = new int[N][N];
 
+            for (int i=0;i<N;i++){
+                st = new StringTokenizer(br.readLine());
+                for (int j=0;j<N;j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int[][] arr90 = new int[N][N];
+            int[][] arr180 = new int[N][N];
+            int[][] arr270 = new int[N][N];
+
+            arr90 = rotateArr(arr,N);
+            arr180 = rotateArr(arr90,N);
+            arr270 = rotateArr(arr180,N);
+            System.out.println("#"+test_case);
+            for (int i=0;i<N;i++){
+                for (int j=0;j<N;j++){
+                    System.out.print(arr90[i][j]);
+                }
+                System.out.print(" ");
+                for (int j=0;j<N;j++){
+                    System.out.print(arr180[i][j]);
+                }
+                System.out.print(" ");
+                for (int j=0;j<N;j++){
+                    System.out.print(arr270[i][j]);
+                }
+                System.out.println();
+            }
+
+
+
+
+        }
+    }
+    private static int[][] rotateArr (int[][] array, int N){
+        int[][] rotated = new int[N][N];
+        for (int i=0;i<N;i++){
+            for (int j=0;j<N;j++){
+                rotated[i][j] = array[N-j-1][i];
+            }
+        }
+        return rotated;
+    }
+}
