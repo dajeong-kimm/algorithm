593ec47fb3ba26bc8a2c5ac2e88f7e4a20623ed9
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-07-10 21:19:29 +0900

    [level 1] Title: 모의고사, Time: 3.60 ms, Memory: 68.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 84.8 MB, 시간: 4.99 ms
+메모리: 68.9 MB, 시간: 3.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 09일 21:45:53
+2024년 07월 10일 21:19:26
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,29 +1,29 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] patterns = {{1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5}};
-
+        int[][] arr = {{1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5}};
+        
         int[] scores = new int[3];
-
-        for(int i=0;i<answers.length;i++){
-            for (int j=0;j< patterns.length;j++){
-                if (answers[i] == patterns[j][i%patterns[j].length]) scores[j]++;
+        
+        for (int i=0;i<answers.length;i++){
+            for (int j=0;j<arr.length;j++){
+                if (answers[i] == arr[j][i%arr[j].length]){
+                    scores[j]++;
+                }
             }
         }
-
+        
         int maxScore = Arrays.stream(scores).max().getAsInt();
-
+        
         ArrayList<Integer> answer = new ArrayList<>();
-
+        
         for (int i=0;i<scores.length;i++){
             if (scores[i] == maxScore) answer.add(i+1);
         }
-
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
-
 }
