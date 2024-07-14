dc911eacdd6396e3403718d75cd698c134b623b4
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-07-14 17:48:46 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 68 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,57 +1,62 @@
-// 1 켜져있음 0 꺼져있음
-// 1~스위치개수
-// 남학생 : 스위치 번호가 자기가 받은 수의 배수이면 바꿈
-// 여학생 : 좌우가 대칭이면서 가장 많은 스위치를 포함하는 구간 (--> 스위치 개수는 항상 홀수)
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] swit = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=0;i<n;i++){
-            swit[i] = Integer.parseInt(st.nextToken());
-        }
-        int m = Integer.parseInt(br.readLine());
-        for (int i=0;i<m;i++){
-            String[] line = br.readLine().split(" ");
-            int gender = Integer.parseInt(line[0]);
-            int number = Integer.parseInt(line[1]);
-            if (gender == 1){
-                int start = number;
-                while (start<=n){
-                    swit[start-1] = change(swit[start-1]);
-                    start += number;
+class Main {
+    private static int[] change_switch(int[] arr, int gen, int num){
+        if (gen == 1){
+            for (int i=num-1;i<arr.length;i+=num){
+                arr[i] ^= 1;
+            }
+
+        }else if (gen == 2){
+            arr[num-1] ^= 1;
+            int k = 1;
+            while (true){
+                if (num-k <1 || num+k>arr.length) break;
+                else{
+                    if (arr[num-1-k] == arr[num-1+k]) {
+                        arr[num-1-k] ^= 1;
+                        arr[num-1+k] ^= 1;
+                        k++;
+                    } else {
+                        break;
+                    }
                 }
             }
-            else {
-                int start = number;
-                swit[start-1] = change(swit[start-1]);
-                int distance = 1;
-                while (true) {
-                    if ((start-distance)>0 && (start+distance)<=n) {
-                        if (swit[start-distance-1] == swit[start+distance-1]) {
-                            swit[start-distance-1] = change(swit[start-distance-1]);
-                            swit[start+distance-1] = change(swit[start+distance-1]);
-                            distance += 1;
-                        }
-                        else break;
-                    } else break;
-                }
-            }
+
         }
-        for (int i=0;i<swit.length;i++){
-            if (i != 0 && i%20 == 0) System.out.println();
-            System.out.print(swit[i]+" ");
-        }
+
+
+        return arr;
     }
-    public static int change(int num){
-        if (num==0) return 1;
-        return 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int NUM = Integer.parseInt(br.readLine());
+        for (int i=0;i<NUM;i++){
+            st = new StringTokenizer(br.readLine());
+            int gen = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            arr = change_switch(arr,gen,num);
+//            for (int j : arr) {
+//                System.out.print(j + " ");
+//            }
+        }
+
+        for (int i=0;i<arr.length;i++){
+            if (i!=0 && i%20 == 0) System.out.println();
+            System.out.print(arr[i]+" ");
+        }
+
+
+
     }
 }
