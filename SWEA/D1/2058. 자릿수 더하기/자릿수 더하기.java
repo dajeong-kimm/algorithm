eb859d0be7a67ca3dc7e4f9cc0f7c0093395337f
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-07-15 06:30:59 +0900

    [D1] Title: 자릿수 더하기, Time: 143 ms, Memory: 19,272 KB -BaekjoonHub

---
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        while (N>0) {
+            answer += N%10;
+            N/= 10;
+        }
+        System.out.println(answer);
+    }
+}
