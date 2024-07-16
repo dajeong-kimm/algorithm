e590a60660948e15c6b660669adcaa6366066a93
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-07-16 22:02:22 +0900

    [level 2] Title: 피보나치 수, Time: 2.42 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/README.md
+++ 프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
 
 ### 성능 요약
 
-메모리: 79.1 MB, 시간: 3.70 ms
+메모리: 76.3 MB, 시간: 2.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 23일 04:31:54
+2024년 07월 16일 22:02:19
 
 ### 문제 설명
 
--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,12 +1,12 @@
 class Solution {
-    public long solution(int n) {
-        long answer = 0;
-        long[] array = new long[n+1];
-        array[0] = 0;
-        array[1] = 1;
-        for (int i=2;i<=n;i++){
-            array[i] = (array[i-1]+array[i-2])%1234567;
+    public int solution(int n) {
+        int[] fibo = new int[n+1];
+        fibo[0] = 0;
+        fibo[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            fibo[i] = (fibo[i-1]+fibo[i-2])%1234567;
         }
-        return array[n];
+        return fibo[n];
     }
 }
