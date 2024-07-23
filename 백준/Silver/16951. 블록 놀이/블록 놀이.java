959a9f48fdbe5a2df9382d78a8676d84e4c36362
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-07-23 23:12:35 +0900

    [Silver IV] Title: 블록 놀이, Time: 108 ms, Memory: 11892 KB -BaekjoonHub

---
+++ 백준/Silver/16951. 블록 놀이/블록 놀이.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+//2번
+class Main {
+    static int answer;
+    static int N;
+    static int K;
+    static int[] arr;
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        for (int i=0;i<N;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        answer = Integer.MAX_VALUE;
+
+        A: for (int i=1;i<=1000;i++){
+            int tmp = 0;
+            int start = i;
+            if (arr[0] != start) tmp++;
+            for (int j=1;j<N;j++){
+                start += K;
+//                if (start>1000) break A;
+                if (arr[j] != start) tmp++;
+            }
+            answer = Math.min(answer, tmp);
+//            System.out.println(tmp);
+        }
+        System.out.println(answer);
+
+
+    }
+
+
+
+}
