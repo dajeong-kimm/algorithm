76dc458c42d178fc0e35f6e08e59cf1e4e3967e1
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-08-19 22:49:10 +0900

    [Silver III] Title: 주유소, Time: 220 ms, Memory: 32440 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,38 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-// N개의 도시
-// 기름통의 크기는 무제한
-// 최소의 비용 계산
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        long[] road = new long[N-1];
-        for (int i=0;i<N-1;i++){
-            road[i] = Long.parseLong(st.nextToken());
-        }
+	static int N;
+	static int[] road;
+	static int[] cost;
+	
+	static int answer;
 
-        st = new StringTokenizer(br.readLine());
-        long[] cost = new long[N];
-        for (int i=0;i<N;i++){
-            cost[i] = Long.parseLong(st.nextToken());
-        }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		road = new int[N-1];
+		cost = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i=0;i<N-1;i++) {
+			road[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i=0;i<N;i++) {
+			cost[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		answer = 0;
+		int cur_cost = cost[0];
+		for (int i=0;i<N-1;i++) {
+			if (cost[i] < cur_cost) {
+				cur_cost = cost[i];
+			}
+			answer += road[i]*cur_cost;
+		}
+		System.out.println(answer);
+		
 
-        long answer = 0;
-        long min_Price = cost[0];
+	}
 
-        for (int i=0;i<N-1;i++){
-            if (min_Price>cost[i]) {
-                min_Price = cost[i];
-            }
-            answer += road[i]*min_Price;
-        }
-
-        System.out.println(answer);
-    }
 }
