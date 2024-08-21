50e7c681fcd85e069d278d660dea0fe48385a5e8
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-08-21 23:23:47 +0900

    [Silver II] Title: 부분수열의 합, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,25 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/**
- * N개의 정수로 이루어진 수열
- * 크기가 양수인 부분수열 중에서
- * 그 수열의 원소를 다 더한 값이 S가 되는 경우의 수
- * @author KOREA
- *
- */
 public class Main {
 	static int N,S;
 	static int[] arr;
+	
+	static int answer;
+	
+	static int[] tmp;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
 		st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		S = Integer.parseInt(st.nextToken());
 		
@@ -28,19 +26,24 @@ public class Main {
 		for (int i=0;i<N;i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		answer = 0;
+//		Arrays.sort(arr);
 		
-		int answer = 0;
-		for (int mask=1;mask<(1<<N);mask++) {
-			int tmp_sum = 0;
-			for (int j=0;j<N;j++) {
-				if ((mask&(1<<j)) != 0) {
-					tmp_sum += arr[j];
-				}
-			}
-			if (tmp_sum == S) answer++;
-		}
+		dfs(0,0);
+		if (S == 0) answer--;
 		System.out.println(answer);
-
 	}
+	
+	public static void dfs(int idx, int cur_num) {
+		if (idx == N) {
+			if (cur_num == S) answer++;
+			return;
+		}
+		
+		dfs(idx+1, cur_num);
+		dfs(idx+1, cur_num + arr[idx]);
+	}
+	
+	
 
 }
