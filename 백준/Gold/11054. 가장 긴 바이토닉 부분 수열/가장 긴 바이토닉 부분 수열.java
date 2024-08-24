7f666fca9c7e25379d4f6e94900472d9c4d07655
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-08-24 09:16:57 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 96 ms, Memory: 12272 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,47 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = scanner.nextInt();
-        }
-        
-        int[] reverseArr = new int[n];
-        for (int i = 0; i < n; i++) {
-            reverseArr[i] = arr[n - i - 1];
-        }
-        
-        int[] increase = new int[n];
-        int[] decrease = new int[n];
-        for (int i = 0; i < n; i++) {
-            increase[i] = 1;
-            decrease[i] = 1;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
-                    increase[i] = Math.max(increase[i], increase[j] + 1);
-                }
-                if (reverseArr[i] > reverseArr[j]) {
-                    decrease[i] = Math.max(decrease[i], decrease[j] + 1);
-                }
-            }
-        }
-        
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = increase[i] + decrease[n - i - 1] - 1;
-        }
-        
-        int maxResult = 0;
-        for (int i = 0; i < n; i++) {
-            maxResult = Math.max(maxResult, result[i]);
-        }
-        
-        System.out.println(maxResult);
-    }
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		int[] r_arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			r_arr[N-1-i] = arr[i];
+		}
+		
+		int[] increase = new int[N];
+		int[] decrease = new int[N];
+		
+		Arrays.fill(increase, 1);
+		Arrays.fill(decrease, 1);
+		
+		for (int i=1;i<N;i++) {
+			for (int j=0;j<i;j++) {
+				if (arr[j] < arr[i]) {
+					increase[i] = Math.max(increase[i], increase[j]+1);
+				}
+			}
+		}
+		
+		for (int i=1;i<N;i++) {
+			for (int j=0;j<i;j++) {
+				if (r_arr[j] < r_arr[i]) {
+					decrease[i] = Math.max(decrease[i], decrease[j]+1);
+				}
+			}
+		}
+		
+		int answer = 0;
+		for (int i=0;i<N;i++) {
+			int tmp = increase[i] + decrease[N-1-i] -1 ;
+			answer = Math.max(answer, tmp);
+		}
+		System.out.println(answer);
+
+	}
+	
+
 }
