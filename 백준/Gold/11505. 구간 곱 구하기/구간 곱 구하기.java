d77ddc0413c9d0534dd92612463b741d049d4650
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-11-13 21:59:11 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 452 ms, Memory: 101532 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -4,18 +4,16 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * 구간 곱 구하기
- * 어떤 N개의 수 
+ * 
  * @author KOREA
  *
  */
 public class Main {
 	static int N,M,K;
 	static long[] arr;
-	
 	static long[] tree;
 	
-	static final int MOD = 1_000_000_007;
+	static int MOD = 1_000_000_007;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -27,69 +25,67 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		arr = new long[N+1];
-		for (int i=1;i<=N;i++) {
+		arr = new long[N];
+		for (int i=0;i<N;i++) {
 			arr[i] = Long.parseLong(br.readLine());
 		}
 		
-		tree = new long[N*4];
-		init(1,N,1);
+		int h = (int) Math.ceil(Math.log(N) / Math.log(2));
+		int treeSize = (1<<(h+1));
 		
+		tree = new long[treeSize];
+		
+		init(1,0,N-1);
+		
+		StringBuilder sb = new StringBuilder();
 		for (int i=0;i<M+K;i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			
-			//b번째 수를 c로 바꾸기
 			if (a == 1) {
-				update(1,N,1,b,c);
+				update(1,0,N-1,b-1,c);
 			}
-			//b부터 c까지의 곱 구하기
-			else {
-				System.out.println(mul(1,N,1,b,c));
+			else if (a == 2) {
+				sb.append(query(1,0,N-1,b-1,c-1)).append("\n");
 			}
 		}
-
+		System.out.println(sb);
+		
 	}
-	public static long init(int start, int end, int node) {
+	static long update(int node, int start, int end, int index, long newValue) {
+		if (index < start || index > end) {
+			return tree[node];
+		}
+		if (start == end) {
+			return tree[node] = newValue % MOD;
+		}
+		
+		int mid = (start + end) /2;
+		return tree[node] = (update(node*2, start, mid, index, newValue) * update(node*2+1, mid+1, end, index, newValue))%MOD;
+	}
+	
+	static long query(int node, int start, int end, int left, int right) {
+		if (right < start || left > end) {
+			return 1;
+		}
+		
+		if (left <= start && right >= end) {
+			return tree[node];
+		}
+		
+		int mid = (start+end)/2;
+		return (query(node*2, start, mid, left, right)*query(node*2+1, mid+1, end, left, right))%MOD;
+	}
+	
+	static long init(int node, int start, int end) {
 		if (start == end) {
 			return tree[node] = arr[start];
 		}
 		
 		int mid = (start+end)/2;
-		//leat 노드가 아니면
-		return tree[node] = (init(start, mid, node*2)*init(mid+1, end, node*2+1))%1_000_000_007;
+		return tree[node] = (init(node*2, start, mid) * init(node*2+1, mid+1, end)) % MOD;
 	}
-	
-	public static long update (int start, int end, int node, int idx, long value) {
-		if (start == end) {
-			return tree[node] = value;
-		}
-		
-		int mid = (start+end)/2;
-		
-		if (idx <= mid) {
-			tree[node] = (update(start, mid, node*2, idx, value) * tree[node*2+1]) % MOD;
-		}
-		else {
-			tree[node] = (tree[node*2] * update(mid+1, end, node*2+1, idx, value)) % MOD;
-		}
-		return tree[node];
-		
-	}
-	
-	public static long mul (int start, int end, int node, int left, int right) {
-		if (left > end || right < start) {
-			return 1;
-		}
-		
-		if (left <= start && end <=right) {
-			return tree[node];
-		}
-		
-		int mid = (start+end)/2;
-		return (mul(start, mid, node*2, left, right) * mul(mid+1, end, node*2+1, left, right))%MOD;
-	}
+
 }
