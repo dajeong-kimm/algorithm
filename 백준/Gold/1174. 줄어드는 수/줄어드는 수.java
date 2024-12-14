62dd53c8ffe62c55ee4911292404db2ef68d7a9c
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2024-12-14 21:13:49 +0900

    [Gold V] Title: 줄어드는 수, Time: 76 ms, Memory: 11872 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static Set<Long> set = new HashSet<>();
+    static List<Long> list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0;i<10;i++){
+            makeNum(i, i);
+        }
+
+        list = new ArrayList<>(set);
+        Collections.sort(list);
+        if (N > list.size()) {
+            System.out.println(-1);
+            return;
+        }
+
+        System.out.println(list.get(N-1));
+
+
+    }
+    static void makeNum(int lastNum, long total) {
+        set.add(total);
+
+        for (int i=0;i<10;i++){
+            if (i < lastNum) {
+                long newNum = total * 10 + i;
+                makeNum(i, newNum);
+            }
+        }
+    }
+}
