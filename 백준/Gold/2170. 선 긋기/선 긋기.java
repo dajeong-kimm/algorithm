00fc43c2c12662297caa7853ba08677a57830634
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2025-03-20 20:00:05 +0900

    [Gold V] Title: 선 긋기, Time: 1212 ms, Memory: 325852 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -1,17 +1,20 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Line implements Comparable<Line>{
-		int start;
-		int end;
+	
+	static class Line implements Comparable<Line> {
+		int start, end;
 		
-		Line(int start, int end){
+		Line(int start, int end) {
 			this.start = start;
 			this.end = end;
 		}
@@ -28,36 +31,32 @@ public class Main {
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
+		
 		List<Line> list = new ArrayList<>();
 		for (int i=0;i<N;i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
-			list.add(new Line(start,end));
+			list.add(new Line(start, end));
 		}
 		
 		Collections.sort(list);
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
 		int answer = 0;
+		int maxValue = Integer.MIN_VALUE;
+		int minValue = Integer.MAX_VALUE;
+		
 		for (int i=0;i<N;i++) {
 			Line cur = list.get(i);
 			
-			//새로 긋기
-			if (cur.start >= max) {
+			if (cur.start >= maxValue) {
+				minValue = cur.start;
+				maxValue = cur.end;
 				answer += (cur.end - cur.start);
-				min = cur.start;
-				max = cur.end;
 			}
-			
-			//중간에 삐져나가지 않고 긋기
-			else if (min <= cur.start && max >= cur.end) {
-				
-			}
-			//중간에 삐져나가서 긋기
-			else {
-				answer += (cur.end - max);
-				max = cur.end;
+			else if (cur.end > maxValue) {
+				minValue = maxValue;
+				maxValue = cur.end;
+				answer += (cur.end - minValue);
 			}
 		}
 		System.out.println(answer);
