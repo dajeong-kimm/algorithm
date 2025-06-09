2303dc8a8f01b783c6eb4a84c28bf363d038cb41
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2025-06-09 19:20:58 +0900

    [Silver II] Title: 예산, Time: 100 ms, Memory: 13000 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,49 +1,53 @@
-// 모든 요청이 배정될 수 있는 경우에는 요청한 금액을 그대로 배정
-// 없는 경우에는 특정한 정수 상한액을 계산하여 그 이상인 예산요청에는 모두 상한액을 배ㅓㅈㅇ
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=0;i<N;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        int M = Integer.parseInt(br.readLine());
+	static int N, M;
+	static int[] arr;
 
-        int start = 1;
-        int end = 0;
-        for (int a : arr) {
-            if (a>end) end = a;
-        }
-        int mid = 0;
-        int answer = 0;
-        while (start<=end) {
-            mid = (start+end)/2;
-            int total = 0;
-            for (int a : arr){
-                if (a>mid) total += mid;
-                else total += a;
-            }
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		int start = 0;
+		int end = 0;
+		st = new StringTokenizer(br.readLine());
+		for (int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			end = Math.max(end, arr[i]);
+		}
+		M = Integer.parseInt(br.readLine());
+		
+		int answer = 0;
+		while (start <= end) {
+			int mid = (start+end)/2;
+			
+			if (isAvail(mid)) {
+				answer = mid;
+				start = mid+1;
+			} else {
+				end = mid-1;
+			}
+		}
+		System.out.println(answer);
+	}
+	
+	static boolean isAvail(int money) {
+		int curMoney = 0;
+		for (int i=0;i<N;i++) {
+			if (arr[i] <= money) {
+				curMoney += arr[i];
+			} else {
+				curMoney += money;
+			}
+			if (curMoney > M) return false;
+		}
+		return true;
+	}
 
-            if (total<=M){
-                start = mid+1;
-                answer = mid;
-            }
-            else {
-                end = mid-1;
-            }
-        }
-
-        System.out.println(answer);
-
-    }
 }
