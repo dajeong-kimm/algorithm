5ee309264a51552683044f0822fea35b7dffc9e9
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2025-06-10 20:55:11 +0900

    [Silver II] Title: 창고 다각형, Time: 80 ms, Memory: 12048 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.java
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.java
@@ -1,59 +1,78 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-class Main {
-    static int[][] arr;
-    static int N;
+public class Main {
+	static class Column implements Comparable<Column> {
+		int x, h;
+		
+		Column(int x, int h) {
+			this.x =x ;
+			this.h = h;
+		}
+		
+		@Override
+		public int compareTo(Column o) {
+			return Integer.compare(x, o.x);
+		}
+		
+	}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int maxHeight = 0;
+		int maxPos = 0;
+		int maxIdx = 0;
+		Column[] arr = new Column[N];
+		for (int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int h = Integer.parseInt(st.nextToken());
+			arr[i] = new Column(x,h);
+		}
+		
+		int answer = 0;
+		
+		Arrays.sort(arr);
+		for (int i=0;i<N;i++) {
+			if (maxHeight < arr[i].h) {
+				maxHeight = arr[i].h;
+				maxPos = arr[i].x;
+				maxIdx = i;
+			}
+		}
+		
+		int curH = arr[0].h;
+		int curX = arr[0].x;
+		for (int i=1;i<=maxIdx;i++) {
+			if (curH <= arr[i].h) {
+				answer += curH*(arr[i].x - curX);
+				curH = arr[i].h;
+				curX = arr[i].x;
+			}
+		}
+		
+		curH = arr[N-1].h;
+		curX = arr[N-1].x;
+		for (int i=N-2;i>=maxIdx;i--) {
+			if (curH <= arr[i].h) {
+				answer += curH * (curX - arr[i].x);
+				curH = arr[i].h;
+				curX = arr[i].x;
+			}
+		}
+		
+		answer += maxHeight;
+		
+		System.out.println(answer);
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][2];
-        for (int i=0;i<N;i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+		
+	}
 
-        }
-        Arrays.sort(arr,Comparator.comparingInt(a -> a[0]));
-
-        int maxHeight = 0 ;
-        int maxIndex = 0;
-        for (int i=0;i<N;i++){
-            if (maxHeight<arr[i][1]) {
-                maxHeight = arr[i][1];
-                maxIndex = i;
-            }
-        }
-
-        int answer = maxHeight;
-        int height = arr[0][1];
-        int cur = arr[0][0];
-
-        for (int i=1;i<maxIndex;i++){
-            if (height<arr[i][1]) {
-                answer += height*(arr[i][0]-cur);
-                height = arr[i][1];
-                cur = arr[i][0];
-            }
-        }
-        answer += (height)*(arr[maxIndex][0]-cur);
-        height = arr[N-1][1];
-        cur = arr[N-1][0];
-        for (int i=N-2;i>maxIndex;i--){
-            if (height < arr[i][1]) {
-                answer += height*Math.abs(arr[i][0]-cur);
-                height = arr[i][1];
-                cur = arr[i][0];
-            }
-        }
-        answer += (height)*Math.abs(cur-arr[maxIndex][0]);
-
-        System.out.println(answer);
-
-
-
-    }
 }
