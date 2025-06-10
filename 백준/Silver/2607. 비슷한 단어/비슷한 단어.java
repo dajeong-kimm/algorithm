abfb3928fac5a72e3bfe5e91c2ca4d15613e84c7
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2025-06-10 09:23:38 +0900

    [Silver II] Title: 비슷한 단어, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.java
@@ -1,46 +1,45 @@
-// 두 개의 단어가 같은 종류의 문자로
-// 같은 문자는 같은 개수 만큼
-// 같은 구성을 갖는 경우, 한 단어에서 한 문자를 빼거나, 하나의 문자를 다른 문자로 바꾸어 ~ 비슷한 단어
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String first = br.readLine();
-        int result = 0;
 
-        for (int i=0;i<N-1;i++){
-            String str = br.readLine();
-            int cnt = 0;
-            int[] word = new int[26];
-            for (int j=0;j<first.length();j++){
-                word[first.charAt(j)-'A']++;
-            }
-            for (int j=0;j<str.length();j++){
-                if (word[str.charAt(j)-'A']>0){
-                    cnt++;
-                    word[str.charAt(j)-'A']--;
-                }
-            }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int answer = 0;
+		String firstWord = br.readLine();
+		
+		for (int i=1;i<N;i++) {
+			int[] first = new int[26];
+			for (int j=0;j<firstWord.length();j++) {
+				first[firstWord.charAt(j)-'A']++;
+			}
+			
+			String word = br.readLine();
+			int same = 0;
+			for (int j=0;j<word.length();j++) {
+				char ch = word.charAt(j);
+				if (first[ch-'A'] > 0) {
+					first[ch-'A']--;
+					same++;
+				} 
+			}
+			
+			if (firstWord.length() == word.length() && (firstWord.length() == same || firstWord.length()-1 == same)) {
+				answer++;
+			} else if (firstWord.length()-1 == word.length() && firstWord.length()-1 == same) {
+				answer++;
+			} else if (firstWord.length()+1 == word.length() && firstWord.length() == same) {
+				answer++;
+			}
+		}
+		
+		System.out.println(answer);
 
-            if (first.length() == str.length() && (first.length() == cnt || first.length()-1 == cnt)) {
-                result++;
-            }
-            else if (first.length() == str.length()-1 && str.length()-1 == cnt){
-                result++;
-            }
-            else if (first.length() == str.length()+1 && str.length()==cnt) {
-                result++;
-            }
+	}
 
-        }
-        System.out.println(result);
-
-
-    }
 }
