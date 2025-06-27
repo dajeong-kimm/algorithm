df37027d246f5634e28edaa7b5d739c35a3235eb
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2025-06-28 00:50:52 +0900

    [level 2] Title: 주식가격, Time: 33.76 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 68.1 MB, 시간: 86.66 ms
+메모리: 73.6 MB, 시간: 33.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 16일 21:16:04
+2025년 06월 28일 00:50:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,21 +1,21 @@
 import java.util.*;
+
 class Solution {
-    public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-        
+    public int[] solution(int[] price) {
+        int n = price.length;
         Stack<Integer> stack = new Stack<>();
         stack.push(0);
+        int[] answer = new int[n];
         
-        for (int i=1;i<n;i++){
-            while(!stack.isEmpty() && prices[i]<prices[stack.peek()]){
+        for (int i=1;i<n;i++) {
+            while(!stack.isEmpty() && price[i] < price[stack.peek()]) {
                 int j = stack.pop();
                 answer[j] = i-j;
             }
             stack.push(i);
         }
         
-        while(!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             int j = stack.pop();
             answer[j] = n-1-j;
         }
