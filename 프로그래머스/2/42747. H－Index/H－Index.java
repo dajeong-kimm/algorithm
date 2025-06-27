bee45e0886cde497a2874c0cbff59a4b60821159
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2025-06-27 19:46:27 +0900

    [level 2] Title: H-Index, Time: 0.92 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        int N = citations.length;
+        Arrays.sort(citations);
+        for (int i=0;i<N;i++) {
+            int h = citations[N-1-i];
+            int upper = i+1;
+            int under = (N-i);
+            answer = Math.max(answer, Math.min(upper, h));
+            
+        }
+        return answer;
+    }
+}
