b04039745142867996353006ab842b73ee40869a
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2025-06-27 15:07:34 +0900

    [level 1] Title: 폰켓몬, Time: 6.51 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 80.3 MB, 시간: 8.22 ms
+메모리: 94.5 MB, 시간: 6.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 18일 22:12:27
+2025년 06월 27일 15:07:32
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,13 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.stream.Collectors;
-
+import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-
-        HashSet<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toCollection(HashSet::new));
-
-        int n = nums.length;
-
-        int k = n/2;
-
-        return Math.min(k, set.size());
+        int N = nums.length;
+        Set<Integer> set = new HashSet<>();
+        for (int i=0;i<N;i++) {
+            set.add(nums[i]);
+        }
+        
+        if (set.size() >= N/2) return N/2;
+        return set.size();
     }
 }
