4e78a95df5488acd1ffe6ec0d2f73ec52cd9cf28
Dajeong Kim <122199259+dajeong-kimm@users.noreply.github.com>
2025-06-27 22:42:15 +0900

    [level 3] Title: N으로 표현, Time: 20.88 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,29 +1,30 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
+        int answer = 0;
         if (N == number) return 1;
         
         Set<Integer> dp[] = new HashSet[9];
-        for (int i=0;i<=8;i++){
+        for (int i=0;i<=8;i++) {
             dp[i] = new HashSet<>();
         }
         
         dp[1].add(N);
         
-        for (int i=2;i<=8;i++){
+        for (int i=2;i<=8;i++) {
             StringBuilder sb = new StringBuilder();
-            for (int j=0;j<i;j++){
+            for (int j=0;j<i;j++) {
                 sb.append(N);
             }
             dp[i].add(Integer.parseInt(sb.toString()));
             
-            for (int j=1;j<i;j++){
+            for (int j=1;j<i;j++) {
                 int k = i - j;
-                for (int num1 : dp[j]){
-                    for (int num2 : dp[k]){
-                        dp[i].add(num1+num2);
-                        dp[i].add(num1-num2);
-                        dp[i].add(num1*num2);
+                for (int num1 : dp[j]) {
+                    for (int num2 : dp[k]) {
+                        dp[i].add(num1 + num2);
+                        dp[i].add(num1 - num2);
+                        dp[i].add(num1 * num2);
                         if (num2 != 0) {
                             dp[i].add(num1/num2);
                         }
@@ -31,7 +32,6 @@ class Solution {
                 }
             }
             
-            //number가 가능한 숫자들 중에 포함되는지
             if (dp[i].contains(number)) return i;
         }
         return -1;
--- 프로그래머스/3/42895. N으로 표현/README.md
+++ 프로그래머스/3/42895. N으로 표현/README.md
@@ -1,10 +1,10 @@
 # [level 3] N으로 표현 - 42895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42895) 
 
 ### 성능 요약
 
-메모리: 77.6 MB, 시간: 31.04 ms
+메모리: 77.8 MB, 시간: 20.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 19일 21:09:47
+2025년 06월 27일 22:42:13
 
 ### 문제 설명
 
